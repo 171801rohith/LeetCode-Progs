a944cbcf1b890270857da4d56094382010ed50af
ROHITH <rohith171801@gmail.com>
2024-11-18 20:45:13 +0530

    PlusOne

--- PlusOne.java
+++ PlusOne.java
@@ -26,30 +26,25 @@
 
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        if (digits[digits.length - 1] < 9) {
-            digits[digits.length - 1]++;
-            return digits;
-        } else {
-            int[] result = new int[digits.length + 1];
-            result[0] = 0;
-            for (int i = 0; i < digits.length; i++) {
-                result[i + 1] = digits[i];
-            }
-            for (int i = result.length - 1; i >= 0; i--) {
-                if(result[i] == 9){
-                    result[i] = 0;
-                }else{
-                    result[i]++;
-                    break;
-                }
-            }
-            if(result[0] == 0){
-                for (int i = 1; i < result.length; i++) {
-                    digits[i - 1] = result[i];
-                }
-                return digits;
-            }
-            return result;
+        int[] result = new int[digits.length + 1];
+        result[0] = 0;
+        for (int i = 0; i < digits.length; i++) {
+            result[i + 1] = digits[i];
         }
+        for (int i = result.length - 1; i >= 0; i--) {
+            if (result[i] == 9) {
+                result[i] = 0;
+            } else {
+                result[i]++;
+                break;
+            }
+        }
+        if (result[0] == 0) {
+            for (int i = 1; i < result.length; i++) {
+                digits[i - 1] = result[i];
+            }
+            return digits;
+        }
+        return result;
     }
 }
