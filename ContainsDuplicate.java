b463ec44a18d0812b85c123c7f97ae9d746c794b
ROHITH <rohith171801@gmail.com>
2025-03-05 18:47:26 +0530

    updated to HashSet

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -17,14 +17,22 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-import java.util.Arrays;
+// import java.util.Arrays;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
-     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 1 ; i++){
-                if(nums[i] == nums[i+1]) return true;
-            }
+    public boolean containsDuplicate(int[] nums) {
+        // Arrays.sort(nums);
+        // for(int i = 0; i < nums.length - 1 ; i++){
+        // if(nums[i] == nums[i+1]) return true;
+        // }
+        // return false;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i))
+                return true;
+            set.add(i);
+        }
         return false;
     }
 }
