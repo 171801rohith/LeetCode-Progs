cea5ed5eb8fdc14021d1bdcd6815410148bd3faa
Rohith M <rohith171801@gmail.com>
2025-07-15 12:50:17 +0530

    insertion Sort

--- Some Problems/SortingAlgorithms.java
+++ Some Problems/SortingAlgorithms.java
@@ -29,6 +29,20 @@ public class SortingAlgorithms {
         }
     }
 
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else
+                    break;
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 8, 7, 6, 5, 4, 3, 2, 1 };
         System.out.println("Before Sorting: " + Arrays.toString(arr));
@@ -37,7 +51,10 @@ public class SortingAlgorithms {
         // bubbleSort(arr);
 
         // 2
-        selectionSort(arr);
+        // selectionSort(arr);
+
+        // 3
+        insertionSort(arr);
 
         System.out.println("After Sorting: " + Arrays.toString(arr));
     }
