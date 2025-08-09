166ff52bf3e6e42d238f7005326e407ee329fea0
Rohith M <rohith171801@gmail.com>
2025-08-09 20:57:59 +0530

    BinarySearch Recursion

--- Some Problems/Recursion.java
+++ Some Problems/Recursion.java
@@ -15,10 +15,26 @@ public class Recursion {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
+    public static int binarySearch(int low, int high, int[] nums, int target) {
+        if (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                return binarySearch(mid + 1, high, nums, target);
+            else
+                return binarySearch(low, mid - 1, nums, target);
+        }
+        return -1;                                                                                           
+    }
+
     public static void main(String[] args) {
         // printNums(6);
-        for (int i = 0; i <= 5; i++) {
-            System.out.print(fibonacci(i) + " ");
-        }
+        // for (int i = 0; i <= 5; i++) {
+        //     System.out.print(fibonacci(i) + " ");
+        // }
+
+        int[] nums = {2, 4, 6, 7, 9, 10, 11};
+        System.out.println(binarySearch(0, nums.length - 1, nums, 9));
     }
 }
