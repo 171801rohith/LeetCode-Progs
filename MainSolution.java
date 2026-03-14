8e1433a49e1bd8c0551a2ec3af85b6eae7eef866
Rohith M <rohith171801@gmail.com>
2026-03-14 21:22:00 +0530

    299. Bulls and Cows

--- MainSolution.java
+++ MainSolution.java
@@ -704,7 +704,11 @@ public class MainSolution {
         // System.out.println("Result: " + rpn.evalRPN(tokens));
 
         // 134
-        CountingBits cb = new CountingBits();
-        System.out.println("Result: " + Arrays.toString(cb.countBits(5)));
+        // CountingBits cb = new CountingBits();
+        // System.out.println("Result: " + Arrays.toString(cb.countBits(5)));
+
+        // 135
+        BullsNCows bnc = new BullsNCows();
+        System.out.println("Result: " + bnc.getHint("1122", "1222"));
     }
 }
