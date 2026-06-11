078f7ae0acfd2f6605df4e7381e2d4e99058f763
Rohith M <rohith171801@gmail.com>
2026-06-11 19:19:02 +0530

    -- 1795. Rearrange Products Table

--- LeetCodeSQLQueries.sql
+++ LeetCodeSQLQueries.sql
@@ -326,3 +326,20 @@ SELECT SELL_DATE, COUNT(DISTINCT PRODUCT) AS NUM_SOLD,
 STRING_AGG(DISTINCT PRODUCT, ',' ORDER BY PRODUCT) AS PRODUCTS
 FROM ACTIVITIES
 GROUP BY SELL_DATE;
+
+-- 1795. Rearrange Products Table
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+
+UNION ALL
+
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+
+UNION ALL
+
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL;
